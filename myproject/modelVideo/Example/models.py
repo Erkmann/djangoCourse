76bb1a3fdf879bb9c88798f7ff9d69d5b250951e
Erkmann <36168066+Erkmann@users.noreply.github.com>
2019-02-26 16:02:29 -0300

    Add files via upload

---
+++ myproject/modelVideo/Example/models.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Company(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name= models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Programer(models.Model):
+    name = models.CharField(max_length=20)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    languages = models.ManyToManyField(Language)
+
+    def __str__(self):
+        return self.name
+
